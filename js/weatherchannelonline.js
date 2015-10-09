8d4b03f0951c280970126591bcee8ed05c13ead0
valtersboze <briinums@gmail.com>
2015-10-09 19:37:44 +0000

    Commit1

---
+++ js/weatherchannelonline.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Javscript file for weatherchannelonline module.
+ */
+
+(function ($) 
+{
+
+	$(document).ready( function()
+	{
+		setTimeout( function () 
+		{
+			$("#block-weatherchannelonline-weatherchannelonline").slideUp();
+		}, 10000);
+	});
+}(jQuery));
